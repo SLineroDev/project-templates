13e2f4ec77aef5205a6352db125d4e3e94f88716
Sergio Linero <SLineroDev@gmail.com>
2022-05-10 13:08:19 +0200

    Vite react ts standard vitest

---
+++ vite-react-standard-vitest/vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') }
+    ]
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    
+  },
+  plugins: [react()]
+})
